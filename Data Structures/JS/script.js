6a374e1508f1479903f2c6d9365b7adc9964f6a5
Arunabh <arunabh.matrix@gmail.com>
2020-03-28 21:59:03 -0500

    New Problem

--- Data Structures/JS/script.js
+++ Data Structures/JS/script.js
@@ -1,3 +1,4 @@
+// /*****************************Sock Merchant**************************/
 // function sockMerchant (arr) {
 // 	let pairs = 0;
 // 	const myDict = {};
@@ -22,6 +23,7 @@
 
 // console.log(sockMerchant([ 10, 20, 20, 10, 10, 30, 50, 10, 20 ]));
 
+// /*****************************JCounting Valleys**************************/
 // function countingValleys (n, s) {
 // 	let currValue = 0;
 // 	let valleyCount = 0;
@@ -40,7 +42,7 @@
 
 // console.log(countingValleys(8, 'UDDDUUDUDUU'));
 
-// // // // Jumping on the Clouds
+// /*****************************Jumping on the Clouds**************************/
 
 // function jumpingOnClouds (cloudsArray) {
 // 	let steps = 0;
@@ -60,7 +62,8 @@
 
 // console.log(jumpingOnClouds([ 0, 0, 1, 0, 0, 1, 0 ]));
 
-// Approach 1: works with small size of strings
+// /*****************************Repeated Strings**************************/
+// //// Approach 1: works with small size of strings
 // function repeatedString (s, n) {
 // 	const initialStr = s;
 // 	let aCount = 0;
@@ -75,7 +78,7 @@
 
 // console.log(repeatedString('aba', 10));
 
-// Approach 2: Efficient and works always (No heap out of memory error)
+// // Approach 2: Efficient and works always (No heap out of memory error)
 // function repeatedString (s, n) {
 // 	const initialElements = [ ...s.slice(0, n % s.length) ].filter((el) => el == 'a').length;
 // 	const repeat = [ ...s ].filter((el) => el == 'a').length * (s.length < n ? parseInt(n / s.length) : 0);
@@ -84,6 +87,7 @@
 
 // console.log(repeatedString('aba', 10));
 
+// /*****************************Hour Glass**************************/
 // function hourglassSum (arr) {
 // 	let max = -99;
 // 	for (let i = 0; i < Math.ceil(arr.length / 2) + 1; i++) {
@@ -119,63 +123,140 @@
 // 	return arr;
 // }
 
-// const arr = [ 1, 2, 3, 4, 5 ];
+// const arr1 = [ 1, 2, 3, 4, 5 ];
 
-// console.log(rotLeft(arr, 4));
+// console.log(rotLeft(arr1, 4));
 
-// function minimumBribes(q) {
+// /*****************************Minimum Bribes**************************/
+// function minimumBribes (q) {
+// 	let bribes = 0,
+// 		i,
+// 		j;
 
-//     let bribes = 0, i, j;
-
-//     for (let i = 0; i < q.length; i++) {
-//         const pos = q[i], at = i + 1;
-//         if (pos - at > 2) { return "Too chaotic" }
-//         for (j = pos - 2; j < i; j++) {
-//             if (q[j] > pos) { bribes++ }
-//         }
-//     }
-//     return bribes;
+// 	for (let i = 0; i < q.length; i++) {
+// 		const pos = q[i],
+// 			at = i + 1;
+// 		if (pos - at > 2) {
+// 			return 'Too chaotic';
+// 		}
+// 		for (j = pos - 2; j < i; j++) {
+// 			if (q[j] > pos) {
+// 				bribes++;
+// 			}
+// 		}
+// 	}
+// 	return bribes;
 // }
 
 // console.log(minimumBribes([ 1, 2, 5, 3, 7, 8, 6, 4 ]));
 
-//-------------Day 2----------------------------.to
-
-// Complete the beautifulDays function below.
+// /*****************************Beautiful Days**************************/
 // function beautifulDays (i, j, k) {
-
-//     let beautifulDaysCount = 0;
+// 	let beautifulDaysCount = 0;
 
 // 	for (let day = i; day < j + 1; day++) {
-
-//         (Math.abs(day - day.toString(10).split('').reverse().join('')) % k) === 0 ? beautifulDaysCount+=1 : null;
-
+// 		Math.abs(day - day.toString(10).split('').reverse().join('')) % k === 0 ? (beautifulDaysCount += 1) : null;
 // 	}
 
-// 	return (beautifulDaysCount);
+// 	return beautifulDaysCount;
 // }
 
 // console.log(beautifulDays(20, 23, 6));
 
+// /*****************************Factorial**************************/
 // const factorial = (n) => {
+// 	if (n === 0 || n === 1) return 1;
 
-//     if(n===0 || n===1) return 1
+// 	return n * factorial(n - 1);
+// };
 
-//     return n*(factorial(n-1))
+// console.log(factorial(3));
+
+// /*****************************Vowels and Consonants**************************/
+// function vowelsAndConsonants (s) {
+// 	const vowels = [ 'a', 'e', 'i', 'o', 'u' ];
+// 	let printVowels = [];
+// 	let printConsts = [];
+
+// 	[ ...s ].forEach((ch) => {
+// 		vowels.includes(ch) ? printVowels.push(ch) : printConsts.push(ch);
+// 	});
+
+// 	[ ...printVowels, ...printConsts ].forEach((el) => console.log(el));
 // }
 
-// console.log(factorial(3))
+// vowelsAndConsonants('abcdefgh');
 
-function vowelsAndConsonants (s) {
-	const vowels = [ 'a', 'e', 'i', 'o', 'u' ];
-	let printVowels = [];
-	let printConsts = [];
+// /*****************************MINIMUM SWAPS 2*********************************
+// *   You are given an unordered array consisting of consecutive integers
+// *   [1, 2, 3, ..., n] without any duplicates. You are allowed to swap
+// *   any two elements. You need to find the minimum number of swaps required
+// *   to sort the array in ascending order.
+// ******************************************************************************/
 
-	[ ...s ].forEach((ch) => {
-		vowels.includes(ch) ? printVowels.push(ch) : printConsts.push(ch);
+// const array1 = [ 4, 3, 1, 2 ];
+// const array2 = [ 1, 3, 5, 2, 4, 6, 7 ];
+
+// function minimumSwaps (arr) {
+// 	// Find the Min element
+// 	let min = arr[0];
+// 	arr.forEach((el) => (el < min ? (min = el) : null));
+// 	// Logic starts here
+// 	let swaps = 0;
+// 	let toSwap = true;
+// 	arr.forEach((el, i) => {
+// 		let curr = el;
+// 		while (curr !== i + min && toSwap) {
+// 			//console.log(i + min, curr);
+// 			[ arr[curr - min], arr[i] ] = [ arr[i], arr[curr - min] ];
+// 			swaps += 1;
+// 			curr = arr[i];
+// 			curr === i + min ? !toSwap : null;
+// 		}
+// 	});
+
+// 	return swaps;
+// }
+
+// console.log(minimumSwaps([ 1, 3, 5, 2, 4, 6, 7 ]));
+
+/*****************************Sock Merchant**************************/
+function checkMagazine (magazine, note) {
+	const magWordsArr = magazine.split(' ');
+	const ranWordsArr = note.split(' ');
+
+	// Initial Check
+	if (magWordsArr.length < ranWordsArr.length) return 'No';
+
+	// Make Magazine words dict with count as Val and element as key
+	const mDict = {};
+	magWordsArr.forEach((el) => {
+		if (mDict[el]) mDict[el] = mDict[el] + 1;
+		else mDict[el] = 1;
 	});
 
-	[ ...printVowels, ...printConsts ].forEach((el) => console.log(el));
+    //Chech for words in ransonNote array
+    for (let i = 0; i < ranWordsArr.length; i++) {
+        const el = ranWordsArr[i];
+        if (mDict[el]&& mDict[el] !== 0) {
+			mDict[el] = mDict[el] - 1;
+			
+		}
+		else return 'No';
+    }
+
+
+	ranWordsArr.forEach((el) => {
+		// if that el is in mDict
+		if (mDict[el]&& mDict[el] !== 0) {
+			mDict[el] = mDict[el] - 1;
+			ret = 'Yes';
+		}
+		else ret = 'No';
+	});
+
+	return 'Yes';
 }
 
-vowelsAndConsonants('abcdefgh');
+console.log(checkMagazine('two times three is not four', 'two times two is four'));
+console.log(checkMagazine('give me one grand today night', 'give one grand today'));
