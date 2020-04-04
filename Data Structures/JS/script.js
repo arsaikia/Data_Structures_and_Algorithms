809248d0599b1da292dfc2d82e12aa8ccd9a00c6
Arunabh <arunabh.matrix@gmail.com>
2020-04-03 20:22:23 -0500

    10 Days of JS :fire:

--- Data Structures/JS/script.js
+++ Data Structures/JS/script.js
@@ -788,3 +788,22 @@
 //     })
 //     return n;
 // }
+
+
+
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+
+    const x= ( (expressions[1] + Math.sqrt( (expressions[1]*expressions[1]) - (16*expressions[0]))) /4 );
+    const y = ( (expressions[1] - Math.sqrt( (expressions[1]*expressions[1]) - (16*expressions[0]))) /4 );
+    
+   return [Math.min(x,y), Math.max(x,y)]
+}
