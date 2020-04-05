2bc10d85b62b05357dd8f4ca8f9d61802bc17e1b
Arunabh <arunabh.matrix@gmail.com>
2020-04-05 01:03:45 -0500

    :fire:

--- Data Structures/JS/dummy.py
+++ Data Structures/JS/dummy.py
@@ -5,29 +5,16 @@ def isHappy(n):
     numArr = []
 
     while (sum != 1):
-
-        print(loopArr, n)
-        
-        
-
         sum = 0
         numArr = []
         while(n > 0):
-            #print(n % 10, n//10, numArr, sum)
             numArr.append(n % 10)
-            
             n = n//10
-
-        for i in numArr:
-            
-            sum += i*i
-            
-        #print(sum);
+        for i in numArr:   
+            sum += i*i 
         if( sum in loopArr): return False
-
         n = sum
         loopArr.append(sum)
-        
     return True
 
 
