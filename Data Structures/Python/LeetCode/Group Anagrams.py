b59c6086091846b33f90c4201360390010770940
Arunabh <arunabh.matrix@gmail.com>
2020-04-12 03:12:42 -0500

    Changes

--- Data Structures/Python/LeetCode/Group Anagrams.py
+++ Data Structures/Python/LeetCode/Group Anagrams.py
@@ -17,3 +17,35 @@ __Note:__
 *  The order of your output does not matter.
 
 '''
+
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        anagList = [] 
+        opList=[]
+
+        for each in strs:
+            asciiVal = 1
+            for ch in each:
+                asciiVal += ord( ch )
+            anagList.append( asciiVal )
+        
+        for i in range(len(anagList)):
+
+        
+        return anagList
+
+
+
+
+
+
+if __name__ == "__main__":
+    
+    input = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+    anagramClass = Solution()
+
+    print(anagramClass.groupAnagrams( input ))
