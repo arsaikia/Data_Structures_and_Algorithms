5ad3c94b4e6b79a2a17ff29e2bce6fa4a5f0cff2
Arunabh <arunabh.matrix@gmail.com>
2020-04-13 04:05:55 -0500

    Initialize: Backspace String Compare

---
+++ Data Structures/Python/LeetCode/Backspace String Compare.py
@@ -0,0 +1,31 @@
+'''
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+
+Note:
+
+1 <= S.length <= 200
+1 <= T.length <= 200
+S and T only contain lowercase letters and '#' characters.
+Follow up:
+
+Can you solve it in O(N) time and O(1) space?
+
+https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/529/week-2/3291/
+
+'''
+from typing import List
+
+
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        pass
+
+
+if __name__ == "__main__":
+    pass
