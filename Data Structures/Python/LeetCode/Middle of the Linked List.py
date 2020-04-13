9f986b763e704105a3ddedef4d4455d0518d2cfe
Arunabh <arunabh.matrix@gmail.com>
2020-04-13 03:57:36 -0500

    Done

--- Data Structures/Python/LeetCode/Middle of the Linked List.py
+++ Data Structures/Python/LeetCode/Middle of the Linked List.py
@@ -28,16 +28,15 @@ from typing import List
 # Definition for singly-linked list.
 
 
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
 class Solution:
     def middleNode(self, head: ListNode) -> ListNode:
-        pass
 
+        slowPointer = fastPointer = head
 
-if __name__ == "__main__":
-    pass
+        while(fastPointer.next and fastPointer.next.next):
+            slowPointer = slowPointer.next
+            fastPointer = fastPointer.next.next
+
+        if(fastPointer.next):
+            return slowPointer.next
+        return slowPointer
