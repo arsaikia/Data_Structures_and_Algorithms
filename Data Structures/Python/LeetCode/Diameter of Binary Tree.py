8817ab3d8a738f4d8dec139b24a271fd01dfc448
Arunabh <arunabh.matrix@gmail.com>
2020-04-14 00:05:40 -0500

    BinaryTree

---
+++ Data Structures/Python/LeetCode/Diameter of Binary Tree.py
@@ -0,0 +1,9 @@
+'''
+Given a binary tree, you need to compute the length of the diameter of the tree. 
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. 
+This path may or may not pass through the root.
+
+https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/529/week-2/3293/
+
+'''
+
