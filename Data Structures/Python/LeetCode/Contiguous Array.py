2abd205a09e35df5eb14078ad0aff92df0a6de9d
Arunabh <arunabh.matrix@gmail.com>
2020-04-14 20:54:51 -0500

    Changes

--- Data Structures/Python/LeetCode/Contiguous Array.py
+++ Data Structures/Python/LeetCode/Contiguous Array.py
@@ -21,8 +21,8 @@ class Solution:
 
         zero = 0
         one = 0
-        c=0
-        g=0
+        c = 0
+        g = 0
 
         for each in nums:
             if(each == 0):
@@ -30,19 +30,16 @@ class Solution:
             else:
                 one += 1
 
-            if( zero == one ): 
-                c +=2
-
-
-
-            if(c%2 == 0): g += c
-
+            if(zero == one):
+                c += 2
+            if(c % 2 == 0):
+                g += c
         return g
 
 
 if __name__ == "__main__":
     sol = Solution()
-    Input = [0,1,1,0,1,1,1,0]
-    Input1 = [0,0,1,0,0,0,1,1]
+    Input = [0, 1, 1, 0, 1, 1, 1, 0]
+    Input1 = [0, 0, 1, 0, 0, 0, 1, 1]
     for i in range(0, len(Input1)):
         print(sol.findMaxLength(Input1[i:]))
