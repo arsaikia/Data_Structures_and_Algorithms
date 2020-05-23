9ebc9a013e193c62b7d63abd1d2df14d8d6626f2
Arunabh <arunabh.matrix@gmail.com>
2020-05-23 00:05:32 -0500

    Done :)

--- Data Structures/Python/LeetCode/Kids With the Greatest Number of Candies.py
+++ Data Structures/Python/LeetCode/Kids With the Greatest Number of Candies.py
@@ -3,8 +3,21 @@ from typing import List
 
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        
+        max = -1
+        isGreatest = []
+
+        for each in candies:
+            if each > max:
+                max = each
+
+        for each in candies:
+            isGreater = False
+            if (extraCandies + each >= max):
+                isGreater = True
+            isGreatest.append(isGreater)
+        return isGreatest
 
 
 if __name__ == "__main__":
-    pass
+    sol = Solution()
+    print(f'The output is : {sol.kidsWithCandies([2,3,5,1,3], 3)}')
