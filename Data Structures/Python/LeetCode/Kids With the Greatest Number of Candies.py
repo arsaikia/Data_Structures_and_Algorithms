a89d870cf4d543b8e2355be72759c7a558cf51da
Arunabh <arunabh.matrix@gmail.com>
2020-05-22 23:21:07 -0500

    Initial

---
+++ Data Structures/Python/LeetCode/Kids With the Greatest Number of Candies.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        
+
+
+if __name__ == "__main__":
+    pass
