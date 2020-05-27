299a65c314ccad39db22b189ba952bddac066469
Arunabh <arunabh.matrix@gmail.com>
2020-05-27 01:48:28 -0500

    Revisited the problem

--- Data Structures/Python/DP/targetAsArrayElementsSum.py
+++ Data Structures/Python/DP/targetAsArrayElementsSum.py
@@ -1,28 +1,34 @@
 
-count = 0
-arr = [2, 4, 6, 10, 12]
 
+'''
+DocString:
+'''
 
-def DP( i, target):
-    global arr
-    global count
-
-    if ( target == 0 ):
-        count += 1
-        return
-    elif ( i >= len(arr)): return 0
-
-    DP( i+1, target-arr[i])
-    DP( i+1, target)
-
-    return
-
-
-
-if __name__ == "__main__":
-
-    DP( 0, 12)
-    print(count)
-
-
+def target_as_array_elements_sum( array, target ):
+       
+    return helper_dp( array, target, 0 )
     
+
+def helper_dp( array, target, start ):
+
+    local_count = 0
+
+    if( target == 0 ):
+        return 1
+    if( start > len(array)-1 ):
+        return 0
+
+    local_count += helper_dp( array, target, start+1 )
+    local_count += helper_dp( array, target-array[start], start+1 )
+
+    return local_count
+
+
+
+
+
+
+
+global count
+arr = [2, 4, 6, 10, 12]
+print(f'Count of ways we can get the target is : {target_as_array_elements_sum(arr, 12)}')
