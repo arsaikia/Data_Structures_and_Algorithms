7beb7872ba9fa534f4b9badc731c70f6835900e7
Arunabh <arunabh.matrix@gmail.com>
2020-05-30 03:54:26 -0500

    par

---
+++ Data Structures/Python/LeetCode/Valid Parentheses.py
@@ -0,0 +1,18 @@
+def isValid(s):
+    myStack = []
+    myDict = {
+        '(': ')',
+        '{': '}',
+        '[': ']'}
+    
+    for each in s:
+        if(each == '(' or each =='{' or each =='['): myStack.append(each)
+        else:
+            if not each == myDict.get(myStack.pop() if myStack else False , None): return False
+    
+    if len(myStack) > 0 : return False
+
+    return True
+
+
+print(isValid(")("))
