15b9ff1c6f96afb8f379ab6c9a6034805a916f55
Arunabh <arunabh.matrix@gmail.com>
2020-05-30 23:06:32 -0500

    L4-Linked List Constructor

---
+++ Data Structures/Python/Algo Expert/LinkedList.py
@@ -0,0 +1,30 @@
+class doublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.val = 0
+    
+    def setHead(self, node):
+        pass
+
+    def setTail(self, node):
+        pass
+
+    def insertBefore(self, node, nodeToInsert):
+        pass
+
+    def insertAfter(self, node, nodeToInsert):
+        pass
+
+    def insertAtPosition(self, position, nodeToInsert):
+        pass
+
+    def removeNodeWithValue(self, value):
+        pass
+
+    def remove(self, node):
+        pass
+
+    def containsNodeWithValue(self, value):
+        pass
+    
