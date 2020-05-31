1cad7c386f96b30fc3e609001b81528b3fbcbb5e
Arunabh <arunabh.matrix@gmail.com>
2020-05-31 15:08:22 -0500

    Done

---
+++ Data Structures/Python/LeetCode/Remove Duplicates from Sorted Array.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        for i in range(len(nums)-1, 0, -1):
+            if nums[i] == nums[i-1]:
+                nums.pop(i)
+
+    def removeDup(self, nums: List[int]) -> int:
+        nums[:] = sorted(set(nums))
+
+
+if __name__ == "__main__":
+    array = [1, 1, 2, 2, 3, 4, 4]
+    array1 = [1, 1, 2, 2, 3, 4, 4]
+
+    sol = Solution()
+    sol.removeDuplicates(array)
+    print(array)
+    sol.removeDup(array1)
+    print(array)
