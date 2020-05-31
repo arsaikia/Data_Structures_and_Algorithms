ba4b86ba6b53bec94e6d41b9f9b8166d07c6a109
Arunabh <arunabh.matrix@gmail.com>
2020-05-31 03:48:27 -0500

    Modified

--- Data Structures/Python/LeetCode/Remove Linked List Elements.py
+++ Data Structures/Python/LeetCode/Remove Linked List Elements.py
@@ -2,30 +2,27 @@
 
 from typing import List
 
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
+
 class Solution:
     def removeElements(self, head: ListNode, val: int) -> ListNode:
+
+        while(head is not None and head.val == val):
+            head = head.next
         if head == None:
             return head
-        
+
         Node = head
-        if(Node.next == None):
-            return head if Node.val != val else None
-        
         while Node.next is not None:
-            nodeToRemove = Node
-            Node = Node.next
-            if nodeToRemove.next.val == val:
-                self.removeBindings(nodeToRemove)
-        
-        return head
-
-        def removeBindings(self, Node):
             nodeToRemove = Node.next
-            Node.next = nodeToRemove.next
+            if nodeToRemove.val == val:
+                Node.next = nodeToRemove.next
+            else:
+                Node = Node.next
 
-
+        return head
