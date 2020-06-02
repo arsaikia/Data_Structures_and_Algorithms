b8cfdff8c5e4bbfccc36326af065b737a4614428
Arunabh <arunabh.matrix@gmail.com>
2020-06-02 15:08:44 -0500

    Changes

--- Data Structures/Python/LeetCode/Numbers Disappeared in Array.py
+++ Data Structures/Python/LeetCode/Numbers Disappeared in Array.py
@@ -1,10 +1,21 @@
 from typing import List
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        pass
+        outputArray = []
+
+        for each in nums:
+
+            if nums[abs(each)-1] > 0:
+                nums[abs(each)-1] *= -1
+
+        for i in range(len(nums)):
+            if nums[i] > 0: outputArray.append(i+1) 
+        return outputArray
+
 
 
 
 
 if __name__ == "__main__":
-    pass
+    sol= Solution()
+    print(sol.findDisappearedNumbers( [1,1,2,2] ))
