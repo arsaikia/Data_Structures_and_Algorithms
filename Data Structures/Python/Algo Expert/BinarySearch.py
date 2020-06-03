8c8c38a3eb1443650e2a461edbab60746a42d611
Arunabh <arunabh.matrix@gmail.com>
2020-06-02 23:53:42 -0500

    Binary Search

--- Data Structures/Python/Algo Expert/BinarySearch.py
+++ Data Structures/Python/Algo Expert/BinarySearch.py
@@ -4,23 +4,22 @@ Iterative Approach:
     Space Complexity : O (1)
 '''
 
+
 def binarySearch(array, target):
 
     start = 0
-    end =  len(array)-1
+    end = len(array)-1
 
-    
-    while( start < end):
+    while(start < end):
         potentialValue = start+end // 2
-        if array[potentialValue] == target: return potentialValue
-
+        if array[potentialValue] == target:
+            return potentialValue
         elif array[potentialValue] < target:
             start = potentialValue
-        
         else:
             end = potentialValue
-    
+
     return -1
 
 
-print(binarySearch([1,2,3,4,5], 3))
+print(binarySearch([1, 2, 3, 4, 5], 3))
