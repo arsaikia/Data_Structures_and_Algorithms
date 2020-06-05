e7de08b5afb07ff4ab01850b5fd0621d5f94ed56
Arunabh <arunabh.matrix@gmail.com>
2020-06-04 23:23:18 -0500

    Approach -1 : Reverse the string and compare :)

--- Data Structures/Python/Algo Expert/Palindrome Check.py
+++ Data Structures/Python/Algo Expert/Palindrome Check.py
@@ -1,3 +1,11 @@
+# Approach 1: Reverse the String
+def reverseStringPalindrome(string):
+    revString = string[::-1]
+    if string == revString:
+        return True
+    return False
+
+
 # O(n) Time || O(1) Space
 def checkPalindrome(string):
     for i in range(len(string)//2):
@@ -8,5 +16,9 @@ def checkPalindrome(string):
 
 
 myString = 'MOROM'
-print(f'The String "{myString}" is a palindrome  ? ',
+print(f'APPROACH-1 > The String "{myString}" is a palindrome  ? ',
+      reverseStringPalindrome(myString))
+
+
+print(f'APPROACH-2 > The String "{myString}" is a palindrome  ? ',
       checkPalindrome(myString))
