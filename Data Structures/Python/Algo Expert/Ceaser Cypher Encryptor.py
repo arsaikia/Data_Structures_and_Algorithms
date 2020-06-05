677c99abf166fb4f143baa8af95e8490343648d8
Arunabh <arunabh.matrix@gmail.com>
2020-06-04 23:59:25 -0500

    Done

---
+++ Data Structures/Python/Algo Expert/Ceaser Cypher Encryptor.py
@@ -0,0 +1,16 @@
+def ceaserCypher( string, key ):
+    newString = []
+    newKey = key % 26
+    for ch in string:
+        newString.append(getNewLetters(ch, newKey))
+    return ''.join(newString)
+
+def getNewLetters( letter, key ):
+    letterCode = ord(letter) + key
+    return chr(letterCode) if letterCode <= 122 else chr(96 + letterCode % 122)
+
+
+myString = 'xyz'
+myKey = 2
+
+print(f'Using ceaser cypher to encode {myString} by key {myKey} gives us : ', ceaserCypher(myString, myKey))
