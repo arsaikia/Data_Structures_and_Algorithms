0a31d2c318417e9abb6c2e7f626d735bfe6f96d5
Arunabh <arunabh.matrix@gmail.com>
2020-06-04 21:40:25 -0500

    Done

--- Data Structures/Python/Algo Expert/find Three Largest Numbers.py
+++ Data Structures/Python/Algo Expert/find Three Largest Numbers.py
@@ -1,19 +1,23 @@
-def findThreeLargestNumbers( array ):
-    threeLargest = [ float('-inf') for i in range(3)]
+
+# O(N) Time || O(1) Space
+def findThreeLargestNumbers(array):
+    threeLargest = [float('-inf') for i in range(3)]
     for each in array:
-        getThreeLargest( threeLargest, each)
+        getThreeLargest(threeLargest, each)
     return threeLargest
 
-def getThreeLargest( array, num):
+
+def getThreeLargest(array, num):
     if num > array[2]:
-        shiftAndUpdate( array, num, 2)
+        shiftAndUpdate(array, num, 2)
     elif num > array[1]:
-        shiftAndUpdate( array, num, 1)
+        shiftAndUpdate(array, num, 1)
     elif num > array[0]:
         shiftAndUpdate(array, num, 0)
     return
 
-def shiftAndUpdate( array, num, idx):
+
+def shiftAndUpdate(array, num, idx):
     for i in range(idx+1):
         if i == idx:
             array[idx] = num
@@ -21,4 +25,6 @@ def shiftAndUpdate( array, num, idx):
             array[i] = array[i+1]
     return
 
-print(findThreeLargestNumbers( [12, 22, 1, 99, 126, 4, 187, 9, 321, 320, 325, 90] ))
+
+print(findThreeLargestNumbers(
+    [12, 22, 1, 99, 126, 4, 187, 9, 321, 320, 325, 90]))
