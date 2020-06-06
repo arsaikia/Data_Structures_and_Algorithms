5938073ae16d7a9f4c9bb8e252c2dd572bf999f8
arsaikia <arunabh.matrix@gmail.com>
2020-06-06 18:29:18 -0500

    Fibonacci All three

--- Data Structures/Python/practice.py
+++ Data Structures/Python/practice.py
@@ -5,7 +5,7 @@ import time as t
 def Naivefib( n ):
     if n == 1: return 0
     elif n == 2: return 1
-    else: return Naivefib(n-1) + Naivefib(n-1)
+    else: return Naivefib(n-1) + Naivefib(n-2)
 
 def Memoizedfib( n , memo={1:0, 2: 1}):
     if n in memo:
@@ -17,14 +17,31 @@ def Memoizedfib( n , memo={1:0, 2: 1}):
 def BottomUpFib( n ):
     firstTwo = [0, 1]
     counter = 3
-    while counter < n:
+    while counter <= n:
         value = firstTwo[0] + firstTwo[1]
         firstTwo[0], firstTwo[1] = firstTwo[1], value
         counter += 1
     return firstTwo[1] if n>1 else firstTwo[0]
 
+num = 30
 start = t.time()
-val = Naivefib(20)
+val = Naivefib(num)
 end = t.time()
-print(f'Naive Fib of 5 is {val} which takes {end-start} seconds!')
-     
+print(f'Naive Fib of {num} is {val} which takes {end-start} seconds!')
+
+start = t.time()
+val = Memoizedfib(num)
+end = t.time()
+print(f'Memoizedfib of {num} is {val} which takes {end-start} seconds!')
+
+start = t.time()
+val = BottomUpFib(num)
+end = t.time()
+print(f'BottomUpFib of {num} is {val} which takes {end-start} seconds!') 
+
+'''
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< RESULTS>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+Naive Fib   of 30 is 514229 which takes 0.1816  seconds!
+Memoizedfib of 30 is 514229 which takes 0.0000  seconds!
+BottomUpFib of 30 is 514229 which takes 0.0000  seconds!
+'''
