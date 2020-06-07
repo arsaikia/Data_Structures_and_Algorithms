ae8a0e6985bdece043f909bc981f8d1a4d444030
arsaikia <arunabh.matrix@gmail.com>
2020-06-06 23:48:05 -0500

    Insertion Sort

---
+++ Data Structures/Python/LeetCode/Insertion Sort.py
@@ -0,0 +1,13 @@
+def InsertionSort( array ):
+    for i in range(1, len(array)):
+        for j in range(i, len(array)):
+            temp = array[j]
+            while j > 0 and array[j-1] > array[j]:
+                array[j] = array[j-1]
+                j -= 1
+            array[j] = temp
+    return array
+
+print(InsertionSort([2,1,3]))
+            
+        
