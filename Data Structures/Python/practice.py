3a79614eb43078d10a87facdbd2a77c461d3e964
arsaikia <arunabh.matrix@gmail.com>
2020-06-06 20:00:56 -0500

    B Search Tre closest value

--- Data Structures/Python/practice.py
+++ Data Structures/Python/practice.py
@@ -1,47 +1,65 @@
-# Fibonnaci
-import time as t
-# Naive
+# # Fibonnaci
+# import time as t
+# # Naive
 
-def Naivefib( n ):
-    if n == 1: return 0
-    elif n == 2: return 1
-    else: return Naivefib(n-1) + Naivefib(n-2)
+# def Naivefib( n ):
+#     if n == 1: return 0
+#     elif n == 2: return 1
+#     else: return Naivefib(n-1) + Naivefib(n-2)
 
-def Memoizedfib( n , memo={1:0, 2: 1}):
-    if n in memo:
-        return memo[n]
+# def Memoizedfib( n , memo={1:0, 2: 1}):
+#     if n in memo:
+#         return memo[n]
+#     else:
+#         memo[n] = Memoizedfib(n-1, memo) + Memoizedfib(n-2, memo)
+#         return memo[n]
+
+# def BottomUpFib( n ):
+#     firstTwo = [0, 1]
+#     counter = 3
+#     while counter <= n:
+#         value = firstTwo[0] + firstTwo[1]
+#         firstTwo[0], firstTwo[1] = firstTwo[1], value
+#         counter += 1
+#     return firstTwo[1] if n>1 else firstTwo[0]
+
+# num = 30
+# start = t.time()
+# val = Naivefib(num)
+# end = t.time()
+# print(f'Naive Fib of {num} is {val} which takes {end-start} seconds!')
+
+# start = t.time()
+# val = Memoizedfib(num)
+# end = t.time()
+# print(f'Memoizedfib of {num} is {val} which takes {end-start} seconds!')
+
+# start = t.time()
+# val = BottomUpFib(num)
+# end = t.time()
+# print(f'BottomUpFib of {num} is {val} which takes {end-start} seconds!') 
+
+# '''
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< RESULTS>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Naive Fib   of 30 is 514229 which takes 0.1816  seconds!
+# Memoizedfib of 30 is 514229 which takes 0.0000  seconds!
+# BottomUpFib of 30 is 514229 which takes 0.0000  seconds!
+# '''
+
+# Find closest in Binary Search tree
+
+def findClosest( tree, target ):
+    closest = float('inf')
+    return findClosestHelper(tree, target, closest)
+
+def findClosestHelper(tree, target, closest):
+    if tree is None:
+        return closest
+    if abs(closest-target) > abs(tree.value - target):
+        closest = tree.value
+    if target < tree.value:
+        return findClosestHelper(tree.left, target, closest)
+    elif target > tree.right:
+        return findClosestHelper(tree.right, target, closest)
     else:
-        memo[n] = Memoizedfib(n-1, memo) + Memoizedfib(n-2, memo)
-        return memo[n]
-
-def BottomUpFib( n ):
-    firstTwo = [0, 1]
-    counter = 3
-    while counter <= n:
-        value = firstTwo[0] + firstTwo[1]
-        firstTwo[0], firstTwo[1] = firstTwo[1], value
-        counter += 1
-    return firstTwo[1] if n>1 else firstTwo[0]
-
-num = 30
-start = t.time()
-val = Naivefib(num)
-end = t.time()
-print(f'Naive Fib of {num} is {val} which takes {end-start} seconds!')
-
-start = t.time()
-val = Memoizedfib(num)
-end = t.time()
-print(f'Memoizedfib of {num} is {val} which takes {end-start} seconds!')
-
-start = t.time()
-val = BottomUpFib(num)
-end = t.time()
-print(f'BottomUpFib of {num} is {val} which takes {end-start} seconds!') 
-
-'''
-<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< RESULTS>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-Naive Fib   of 30 is 514229 which takes 0.1816  seconds!
-Memoizedfib of 30 is 514229 which takes 0.0000  seconds!
-BottomUpFib of 30 is 514229 which takes 0.0000  seconds!
-'''
+        return closest
