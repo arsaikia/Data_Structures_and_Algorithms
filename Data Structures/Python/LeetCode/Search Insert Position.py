fc0b91f6df07c9f3d12c4ef25cfe6fefb7139048
arsaikia <arunabh.matrix@gmail.com>
2020-06-10 23:38:39 -0500

    Using Binary Search

--- Data Structures/Python/LeetCode/Search Insert Position.py
+++ Data Structures/Python/LeetCode/Search Insert Position.py
@@ -1,5 +1,5 @@
 # Inefficient
-def searchInsertPosition(nums, target):
+def searchInsertPositionX(nums, target):
     idx = 0
     for each in nums:
         if each == target: return nums.index(each)
@@ -8,9 +8,20 @@ def searchInsertPosition(nums, target):
     return idx
 
 # Using Binary Search
-
+def searchInsertPosition(nums, target):
+    start, end = 0, len(nums)
+    while start < end:
+        
+        potentialValue = (start+end)//2
+        if target < nums[potentialValue] :
+            end = potentialValue
+        elif target > nums[potentialValue] :
+            start = potentialValue+1
+        else:
+            return potentialValue
+    return start
 
 
 nums = [1,3,5,6]
-target = 10
+target = 9
 print(searchInsertPosition(nums, target))
