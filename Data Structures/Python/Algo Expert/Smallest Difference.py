19ab78a4adb38b7c4eafa277545a456dcf3e2f45
arsaikia <arunabh.matrix@gmail.com>
2020-06-16 23:16:23 -0500

    ...

--- Data Structures/Python/Algo Expert/Smallest Difference.py
+++ Data Structures/Python/Algo Expert/Smallest Difference.py
@@ -1,21 +1,27 @@
 def smallestDifference( array1, array2 ):
-    array1.sort()
-    array2.sort()
+    array1.sort() 
+    array2.sort() 
     idxOne, idxTwo = 0, 0
     smallestCurrent = float('inf')
     smallestGlobal = float('inf')
     
     while idxOne < len(array1) and idxTwo < len(array2):
-        smallestCurrent = min(smallestCurrent, abs(array1[idxOne] - array2[idxTwo]))
-        if array1[idxOne] < array2[idxTwo]:
-            idxOne += 1
-        elif array1[idxOne] > array2[idxTwo]:
+        firstElement = array1[idxOne]
+        secondElement = array2[idxTwo]
+        if firstElement > secondElement:
+            smallestCurrent = min(smallestCurrent, firstElement-secondElement)
             idxTwo += 1
+        elif secondElement > firstElement:
+            smallestCurrent = min(smallestCurrent, secondElement-firstElement)
+            idxOne += 1
         else:
-            return array1[idxOne],  array2[idxTwo]
-        smallestGlobal = min(smallestCurrent, smallestGlobal)
-    return smallestGlobal
+            return [firstElement, secondElement]
+        
+        if smallestCurrent < smallestGlobal:
+            smallestGlobal = smallestCurrent
+            smallestPair = [firstElement, secondElement]
+    return smallestPair
 
 
-print(smallestDifference([1, 3, 15, 11, 2],[23, 127, 235, 19, 8]))
+print(smallestDifference([1, 3, 15, 11, 2],[23, 127, 235, 19, 8, 11]))
         
