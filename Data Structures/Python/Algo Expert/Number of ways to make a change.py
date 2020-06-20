c6c08718602e076fe3100242e04b60ec6cbd3389
arsaikia <arunabh.matrix@gmail.com>
2020-06-20 12:44:29 -0500

    Num of ways to make change!

---
+++ Data Structures/Python/Algo Expert/Number of ways to make a change.py
@@ -0,0 +1,14 @@
+# O(n) Time || O(n) Space
+def numOfWaysToMakeChange(n, denoms):
+    ways = [0 for i in range(n+1)]
+    ways[0] = 1
+    for denom in denoms:
+        for amount in range(1, n+1):
+            if denom <= amount:
+                ways[amount] += ways[amount-denom]
+    return ways[-1]
+
+
+n = 10
+denoms = [1, 5, 10, 15]
+print(numOfWaysToMakeChange(n, denoms))
