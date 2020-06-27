3ea539f4133f7f5803c27509f5b505169b6d4a0b
arsaikia <arunabh.matrix@gmail.com>
2020-06-26 21:20:15 -0500

    BST Traversal

---
+++ Data Structures/Python/Algo Expert/BST Traversal.py
@@ -0,0 +1,27 @@
+# O(n) Time || O(n) Space
+def inorderTraversal(tree, array):
+    if tree is not None:
+        inorderTraversal(tree.left, array)
+        array.append(tree.value)
+        inorderTraversal(tree.right, array)
+    return array
+
+# O(n) Time || O(n) Space
+
+
+def preorderTraversal(tree, array):
+    if tree is not None:
+        array.append(tree.value)
+        preorderTraversal(tree.left, array)
+        preorderTraversal(tree.right, array)
+    return array
+
+# O(n) Time || O(n) Space
+
+
+def postorderTraversal(tree, array):
+    if tree is not None:
+        postorderTraversal(tree.left, array)
+        postorderTraversal(tree.right, array)
+        array.append(tree.value)
+    return array
