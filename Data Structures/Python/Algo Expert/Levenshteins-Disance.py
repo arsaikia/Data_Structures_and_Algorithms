4205d2f29564b2f054c319db276f41ebbbbf427d
arsaikia <arunabh.matrix@gmail.com>
2020-07-03 03:03:00 -0500

    ___

--- Data Structures/Python/Algo Expert/Levenshteins-Disance.py
+++ Data Structures/Python/Algo Expert/Levenshteins-Disance.py
@@ -3,15 +3,16 @@ def levenshteinDistance(str1, str2):
     for i in range(1, len(str2)+1):
         edits[i][0] = edits[i-1][0] + 1
     for i in range(len(str2) + 1):
-        for j in range(len(str1)+1):
+        for j in range(len(str1)
+                       + 1):
             if str2[i-1] == str1[j-1]:
                 edits[i][j] = edits[i-1][j-1]
             else:
                 edits[i][j] = 1 + \
                     min(edits[i-1][j], edits[i][j-1], edits[i-1][j-1])
 
-
     return edits[-1][-1]
+#
 
 
 print(levenshteinDistance('abc', 'yabc'))
