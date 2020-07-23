024077f3f8f70c4e3437351c0ea94861751d05f1
arsaikia <arunabh.matrix@gmail.com>
2020-07-22 21:47:30 -0500

    ..

--- Data Structures/Python/practice.py
+++ Data Structures/Python/practice.py
@@ -87,5 +87,19 @@ PRACTICE
 # [2, 4, 6, 8, 12]  --> 8
 
 
-def branchSums():
-    pass
+class BinaryTree:
+         def __init__(self, value):
+                self.value = value
+                self.left = None
+                self.right = None
+        def branchSums(self, root):
+                sums = []
+                self.calculateBranchSums(root, 0, sums)
+        def calculateBranchSums(self, node, runningSum, sums):
+                 if node is None:
+                         return
+                runningSum += node.value
+                 if node.left is None and node.right is None:
+                         return sums.append(runningSum)
+                self.calculateBranchSums(node.left, runningSum, sums)
+                self.calculateBranchSums(node.right, runningSum, sums)
