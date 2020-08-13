a46d5454ebbe0b4412cbbc6603674ee00643775a
arsaikia <arunabh.matrix@gmail.com>
2020-08-13 04:17:16 -0500

    min heap construction

---
+++ Data Structures/Python/Algo Expert/Min Heap Construction.py
@@ -0,0 +1,54 @@
+class MinHeap:
+    def __init__(self, array):
+        self.heap = self.buildHeap(array)
+
+    def peek(self):
+        return self.heap[0]
+
+    def swap(self, heap, i, j):
+        heap[i], heap[j] = heap[j], heap[i]
+        return
+
+    def insert(self, value):
+        self.heap.append(value)
+        self.siftUp(self.heap, len(self.heap)-1)
+
+    def remove(self):
+        self.swap(self.heap, 0, len(self.heap)-1)
+        toRemove = self.heap.pop()
+        self.siftDown(self.heap, 0, len(self.heap)-1)
+        return toRemove
+
+    def siftUp(self, heap, currentIdx):
+        parentIdx = (currentIdx-1) // 2
+        while currentIdx > 0 and heap[currentIdx] < heap[parentIdx]:
+            self.swap(heap, currentIdx, parentIdx)
+            currentIdx = parentIdx
+            parentIdx = (currentIdx-1) // 2
+    
+    def siftDown(self, heap, currentIdx, endIdx):
+        childOneIdx = (currentIdx*2) + 1
+        while childOneIdx < endIdx:
+            childTwoIdx = (currentIdx*2) + 2 if (currentIdx*2) + 2 <= endIdx else -1
+            if childTwoIdx != -1 and heap[childTwoIdx] < heap[childOneIdx]:
+                idxToSwap = childTwoIdx
+            else:
+                idxToSwap = childOneIdx
+            if heap[currentIdx] > heap[idxToSwap]:
+                self.swap(heap, currentIdx, idxToSwap)
+                currentIdx = idxToSwap
+                childOneIdx = (currentIdx*2)+1
+            else:
+                return
+    
+    def buildHeap(self, array):
+        firstParentIdx = len(array)-2 // 2
+        for currentIdx in reversed(range(firstParentIdx+1)):
+            self.siftDown(array, currentIdx, len(array)-1)
+        return array
+                    
+        
+
+if __name__ == "__main__":
+    minHeap = MinHeap([48, 12, 24, 7, 8, -5, 24, 391, 24, 56, 2, 6, 8, 41])
+    print(minHeap.peek())
