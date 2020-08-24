2c60dfc860331df079ea327a6722a4ee98cffdf2
arsaikia <arunabh.matrix@gmail.com>
2020-08-23 20:09:43 -0500

    ..

--- Data Structures/Python/practice.py
+++ Data Structures/Python/practice.py
@@ -133,3 +133,82 @@ def isDecreasing(arr):
         if arr[idx] > arr[idx-1]:
             return False
     return True
+
+
+print(1 in {1: True, 2: True, 3: True, 4: True})
+
+
+# Do not edit the class below except for
+# the insert, contains, and remove methods.
+# Feel free to add new properties and methods
+# to the class.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        curr = self
+        while True:
+			if value < curr.value:
+				if curr.left is None:
+					curr.left = BST(value)
+					break
+				else:
+					curr = curr.left
+			else:
+				if curr.right is None:
+					curr.right = BST(value)
+					break
+				else:
+					curr = curr.right
+        return self
+
+    def contains(self, value):
+        curr = self
+        while curr is not None:
+			if value < curr.value:
+				curr = curr.left
+			elif value > curr.value:
+				curr = curr.right
+			else: 
+                return True
+	    return False
+
+    def remove(self, value, parent = None):
+        curr = self
+        while curr is not None:
+			if value < curr.value:
+				parent = curr
+				curr = curr.left
+			elif value > curr.value:
+				parent = curr
+				curr = curr.right
+			else:
+				if curr.left is not None and curr.right is not None:
+					curr.value = curr.right.getMinValue()
+					curr.right.remove(curr.value, curr)
+				elif parent is None:
+					if curr.left is not None:
+						curr.value = curr.left.value
+						curr.right = curr.left.right
+						curr.left = curr.left.left
+					elif curr.right is not None:
+						curr.value = curr.right.value
+						curr.left = curr.right.left
+						curr.right = curr.right.right
+					else:
+						curr = None
+				elif parent.left == curr:
+					parent.left = curr.left if curr.left is not None else curr.right
+				elif parent.right == curr:
+					parent.left = curr.left if curr.left is not None else curr.right
+				break
+        return self
+	
+	def getMinValue(self):
+		curr = self
+		while curr.left is not None:
+			curr = curr.left
+   		return curr.value
