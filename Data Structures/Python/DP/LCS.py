5ce96287046939b0fe8fafcc2c7e939a0d4d5c9a
arsaikia <arunabh.matrix@gmail.com>
2020-09-01 02:43:47 -0500

    ..

--- Data Structures/Python/DP/LCS.py
+++ Data Structures/Python/DP/LCS.py
@@ -8,7 +8,7 @@ def LCS(s1, s2, m, n, memo):
         # print("\nHERE", memo)
         return memo[m][n]
     else:
-        if s1[m-1] == s2[n-1]:
+        if s1[m] == s2[n]:
             result = 1 + LCS(s1, s2, m-1, n-1, memo)
         else:
             result = max(LCS(s1, s2, m-1, n, memo), LCS(s1, s2, m, n-1, memo))
@@ -30,7 +30,7 @@ def longestCommonSubsequence(str1, str2):
 
 string1 = 'BATD'
 string2 = 'ABACD'
-memo = [[None for _ in range(len(string2)+1)] for _ in range(len(string1)+1)]
+memo = [[None for _ in range(len(string2))] for _ in range(len(string1))]
 # print(memo)
 print(LCS(string1, string2, len(string1)-1, len(string2)-1, memo))
 
