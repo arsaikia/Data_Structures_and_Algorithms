b120dc11b377c65710daf74b745fcf3364a102a0
arsaikia <arunabh.matrix@gmail.com>
2020-09-02 23:34:16 -0500

    O(n) Time | O(1) Space

--- Data Structures/Python/Algo Expert/Kadane Maximum Subarray Sum.py
+++ Data Structures/Python/Algo Expert/Kadane Maximum Subarray Sum.py
@@ -6,22 +6,25 @@ Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 '''
 
-# O(n) Time | O(n) Space 
-def maxSubarraySum( array ):
+# O(n) Time | O(n) Space
+def maxSubarraySum(array):
     maxSum = array[:]
     for i in range(1, len(array)):
         maxSum[i] = max(maxSum[i], maxSum[i] + maxSum[i - 1])
     return max(maxSum)
 
-# O(n) Time | O(1) Space 
-def kadane( array ):
-    currMax, globalMax = array[0]
+
+
+# O(n) Time | O(1) Space
+def kadane(array):
+    currMax, globalMax = array[0], array[0]
     for i in range(1, len(array)):
-        currMax = max(currMax, currMax + array[i])
+        currMax = max(array[i], currMax + array[i])
         globalMax = max(currMax, globalMax)
     return globalMax
 
 
-
 if __name__ == "__main__":
-    print(maxSubarraySum([-2,1,-3,4,-1,2,1,-5,4]))
+    array = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    print(maxSubarraySum(array))
+    print(kadane(array))
