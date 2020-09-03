b5078e2a201d0df844a63c367a8572a5127e5c4f
arsaikia <arunabh.matrix@gmail.com>
2020-09-03 02:20:04 -0500

    Isomorphic Strings

--- Data Structures/Python/LeetCode/Word Pattern.py
+++ Data Structures/Python/LeetCode/Word Pattern.py
@@ -9,25 +9,25 @@ Here follow means a full match, such that there is a bijection between a letter 
 
 class Solution:
     def wordPattern(self, pattern: str, string: str) -> bool:
-        
-        if string == "" or pattern == "" :
+
+        if string == "" or pattern == "":
             return False
-        
+
         p = list(pattern)
         s = string.split(" ")
-        if len(p) != len(s): return False
-        
+        if len(p) != len(s):
+            return False
+
         cache = {}
         for i in range(len(p)):
             if p[i] not in cache and s[i] not in cache.values():
                 cache[p[i]] = s[i]
-                
+
         print(cache)
         for idx, each in enumerate(p):
             if each not in cache:
                 return False
             if cache[each] != s[idx]:
                 return False
-            
+
         return True
-            
