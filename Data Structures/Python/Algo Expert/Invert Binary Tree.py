61dea89cf89253d1a798d83efaee3dd40ce18384
arsaikia <arunabh.matrix@gmail.com>
2020-09-02 22:37:32 -0500

    # O(n) Time | O(n) Space

--- Data Structures/Python/Algo Expert/Invert Binary Tree.py
+++ Data Structures/Python/Algo Expert/Invert Binary Tree.py
@@ -1,11 +1,31 @@
 from collections import deque
 
+
+
+
+
 # Recursive
-# O(n) Time | O(n) Space
-def invertBinaryTree( node ):
-    if node is None: return
+# O(n) Time | O(logn) Space
+def invertBinaryTree(node):
+    if node is None:
+        return
     invertBinaryTree(node.left)
     invertBinaryTree(node.right)
     node.left, node.right = node.right, node.left
 
+
+
+
+
 # Iterative
+# O(n) Time | O(n) Space
+def invertTree(root):
+    dq = deque([root])
+    while len(dq):
+        node = dq.popleft()
+        if node is None:
+            continue
+
+        node.left, node.right = node.right, node.left
+        dq.append(node.left)
+        dq.append(node.right)
