ff2c8a81a833c2ab4da43af472a074a66e918b3e
arsaikia <arunabh.matrix@gmail.com>
2020-09-04 01:24:12 -0500

    O(n^2) Time | O(n^2) Space

--- Data Structures/Python/Algo Expert/SuffixTrie.py
+++ Data Structures/Python/Algo Expert/SuffixTrie.py
@@ -1,18 +1,28 @@
+from pprint import pprint as pprint
+
+
 class SuffixTrie:
     def __init__(self, string):
         self.root = {}
         self.endSymbol = "*"
         self.populateSuffixTrieFrom(string)
 
+    # O(n^2) Time | O(n^2) Space
     def populateSuffixTrieFrom(self, string):
         for i in range(len(string)):
             self.buildTrieStartingAt(i, string)
     
     def buildTrieStartingAt(self, idx, string):
-        pass
+        node = self.root
+        for j in range(idx, len(string)):
+            if string[j] not in node:
+                node[string[j]] = {}
+            node = node[string[j]]
+        node[self.endSymbol] = True
 
+    # O(m) Time | O(1) Space
     def contains(self, string):
-        node = root
+        node = self.root
         for i in range(len(string)):
             currLetter = string[i]
             if currLetter in node:
@@ -24,4 +34,8 @@ class SuffixTrie:
     
     
 if __name__ == "__main__":
-    pass
+    ST = SuffixTrie("APPLE")
+    print(ST.contains("APPLE"))
+
+    pprint(ST.root, width = 1)
+    
