fd87312073d77769df6eaa2ae526efdf22b3239a
arsaikia <arunabh.matrix@gmail.com>
2020-09-06 22:32:02 -0500

    longestPalindome

--- Data Structures and Algorithms/Amazon Interview Questions/Longest Plaindromic Substring.py
+++ Data Structures and Algorithms/Amazon Interview Questions/Longest Plaindromic Substring.py
@@ -12,10 +12,28 @@ Output: "bb"
 
 string = 'abcd'
 
-op = []
 
-for i in range(len(string)):
-    for j in range(1, len(string)+1):
-        op.append(string[i:j])
-        
-print(op)
+def longestPalindome(string):
+    globalLongest = [0, 1]
+
+    for i in range(len(string)):
+        odd = getPalindrome(string, i - 1, i + 1)
+        even = getPalindrome(string, i - 1, i)
+        currLongest = max(odd, even, key=lambda x: x[1] - x[0])
+        globalLongest = max(currLongest, globalLongest,
+                            key=lambda x: x[1] - x[0])
+    return string[globalLongest[0]: globalLongest[1]]
+
+
+def getPalindrome(string, left, right):
+
+    while left >= 0 and right < len(string):
+        if string[left] == string[right]:
+            left -= 1
+            right += 1
+        else:
+            break
+    return [left + 1, right]
+
+
+print(longestPalindome("aabacaxacddsdderf"))
