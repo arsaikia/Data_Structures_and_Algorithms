a67d16a68ce453b1f82997dca60e305a4e343003
arsaikia <arunabh.matrix@gmail.com>
2020-09-17 00:46:59 -0500

    BST # Average : O(log(n)) Time | O(log(n)) Space # Worst: O(n) Time | O(n) Space

--- Data Structures and Algorithms/Python/Revision/Revision_Medium_1.py
+++ Data Structures and Algorithms/Python/Revision/Revision_Medium_1.py
@@ -26,10 +26,9 @@ def spiralTraverse(matrix):
 
     return traversal
 
+
 # ----------------------------------------< Longest Peak >-----------------------------------------
 # O(N) Time | O(N) Space
-
-
 def longestPeak(array):
     peaks = getPeaks(array)
     return getLongestPeak(array, peaks)
@@ -117,10 +116,9 @@ def uniqueThreeSum(array, target):
                 start += 1
     return [list(triplet) for triplet in triplets]
 
- # ----------------------------------------< Smallest Difference >-----------------------------------------
+
+# ----------------------------------------< Smallest Difference >-----------------------------------------
 # O(n log(n) + m log(m)) Time | O(1) Space
-
-
 def smallestDifference(arrOne, arrTwo):
     arrOne.sort()
     arrTwo.sort()
@@ -145,10 +143,9 @@ def smallestDifference(arrOne, arrTwo):
 
     return elements
 
- # ----------------------------------------< Move Elements To End >-----------------------------------------
+
+# ----------------------------------------< Move Elements To End >-----------------------------------------
 # O(n) Time | O(1) Space
-
-
 def moveElementsToEnd(array, num):
     start = 0
     end = len(array) - 1
@@ -161,15 +158,14 @@ def moveElementsToEnd(array, num):
             end -= 1
     return array
 
- # ----------------------------------------< Monotonic >-----------------------------------------
 
-
+# ----------------------------------------< Monotonic >-----------------------------------------
+# O(n) Time | O(1) Space
 def isMonotonnic(array):
     return validateMonotonicity(array, "increasing") or validateMonotonicity(array, "indecreasing")
 
 
 def validateMonotonicity(array, isType):
-
     for i in range(1, len(array)):
         if compare(array[i - 1], array[i], isType):
             return False
@@ -182,6 +178,99 @@ def compare(num1, num2, isType):
 
 def swap(array, i, j):
     array[i], array[j] = array[j], array[i]
+
+
+# ----------------------------------------< BST CONSTRUCTION >-----------------------------------------
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+    # O(n) Time | O(1) Space
+    def printTree(self):
+        curr = self
+        if curr is None:
+            return
+
+        else:
+            if curr.left is not None:
+                curr.left.printTree()
+            if curr.right is not None:
+                curr.right.printTree()
+        print(curr.value)
+
+    # Average : O(log(n)) Time | O(log(n)) Space
+    # Worst: O(n) Time | O(n) Space
+    def insert(self, value):
+        curr = self
+        while True:
+            if value < curr.value:
+                if curr.left is None:
+                    curr.left = BST(value)
+                    break
+                else:
+                    curr = curr.left
+            else:
+                if curr.right is None:
+                    curr.right = BST(value)
+                    break
+                else:
+                    curr = curr.right
+        return self
+    
+    # Average : O(log(n)) Time | O(log(n)) Space
+    # Worst: O(n) Time | O(n) Space
+    def contains(self, value, parent=None):
+        curr = self
+        while curr is not None:
+            if value < curr.value:
+                curr = curr.left
+            elif value > curr.value:
+                curr = curr.right
+            else:
+                return True
+        return False
+
+    # Average : O(log(n)) Time | O(log(n)) Space
+    # Worst: O(n) Time | O(n) Space
+    def remove(self, value, parent=None):
+        curr = self
+        while curr is not None:
+            if value < curr.value:
+                parent = curr
+                curr = curr.left
+            elif value > curr.value:
+                parent = curr
+                curr = curr.right
+            else:
+                if curr.left is not None and curr.right is not None:
+                    curr.value = curr.right.getMinValue()
+                    curr.right.remove(curr.value, curr)
+                elif parent is None:
+                    if curr.left is not None:
+                        curr.value = curr.left.value
+                        curr.right = curr.left.right
+                        curr.left = curr.left.left
+                    elif curr.right is not None:
+                        curr.value = curr.right.value
+                        curr.left = curr.right.left
+                        curr.right = curr.right.right
+                    else:
+                        return None
+                elif curr == parent.left:
+                    parent.left = curr.left if curr.left is not None else curr.right
+                elif curr == parent.right:
+                    parent.right = curr.left if curr.left is not None else curr.right
+                break
+        return self
+
+    def getMinValue(self):
+        curr = self
+        while curr.left is not None:
+            curr = curr.left
+        return curr.value
 
 
 if __name__ == "__main__":
@@ -211,4 +300,12 @@ if __name__ == "__main__":
     '''Move to end'''
     # print(moveElementsToEnd([2, 1, 2, 2, 2, 3, 4, 2], 2))
 
-    print(isMonotonnic([-1, -5, -10, -1100, -1100, -1101, -1102, -9001]))
+    # print(isMonotonnic([-1, -5, -10, -1100, -1100, -1101, -1102, -9001]))
+
+    bst = BST(10)
+    bst.insert(30).insert(5).insert(2).insert(12).insert(20)
+    bst.printTree()
+    print(bst.contains(12))
+    bst.remove(12)
+    print("\n")
+    bst.printTree()
