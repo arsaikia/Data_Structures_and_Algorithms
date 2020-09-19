db01cfb3bd1718cd737efa770e2b37d185accd97
arsaikia <arunabh.matrix@gmail.com>
2020-09-19 02:54:06 -0500

    ...

--- Data Structures and Algorithms/Python/Algo Expert/Very Hard/Apartment Hunting.py
+++ Data Structures and Algorithms/Python/Algo Expert/Very Hard/Apartment Hunting.py
@@ -1,8 +1,11 @@
 # O(br) Time | O(br) Space
 def apartmentHunting(blocks, reqs):
-    minDistanceFromBlocks = list(map( lambda req: getMinDistance(blocks, req) , reqs)) 
-    maxDistancesAtBlocks = getMaxDistancesAtBlocks(blocks, minDistanceFromBlocks)
+    minDistanceFromBlocks = list(
+        map(lambda req: getMinDistance(blocks, req), reqs))
+    maxDistancesAtBlocks = getMaxDistancesAtBlocks(
+        blocks, minDistanceFromBlocks)
     return getIdxAtMinValue(maxDistancesAtBlocks)
+
 
 def getMinDistance(blocks, req):
     minDistance = [0 for block in blocks]
@@ -15,16 +18,18 @@ def getMinDistance(blocks, req):
     for i in reversed(range(len(blocks))):
         if blocks[i][req]:
             closestReqIdx = i
-        minDistance[i] = min( (minDistance[i]) ,abs(closestReqIdx - i) )
+        minDistance[i] = min((minDistance[i]), abs(closestReqIdx - i))
     return minDistance
 
 
 def getMaxDistancesAtBlocks(blocks, minDistanceFromBlocks):
     maxDistancesAtBlocks = [0 for block in blocks]
     for i in range(len(blocks)):
-        minDistanceAtBlock = list(map( lambda distance : distance[i] , minDistanceFromBlocks))
+        minDistanceAtBlock = list(
+            map(lambda distance: distance[i], minDistanceFromBlocks))
         maxDistancesAtBlocks[i] = max(minDistanceAtBlock)
     return maxDistancesAtBlocks
+
 
 def getIdxAtMinValue(array):
     idxAtValue = 0
@@ -35,10 +40,6 @@ def getIdxAtMinValue(array):
             minValue = currValue
             idxAtValue = i
     return idxAtValue
-
-
-
-
 
 
 if __name__ == "__main__":
