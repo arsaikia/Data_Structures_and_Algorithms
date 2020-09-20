ad4c6f47994f8a76550ccda9cc20a5fe2cf4f60e
arsaikia <arunabh.matrix@gmail.com>
2020-09-19 21:56:29 -0500

    # O(n^2) Time | O(n) Space

--- Data Structures and Algorithms/Python/DP/WordBreak.py
+++ Data Structures and Algorithms/Python/DP/WordBreak.py
@@ -1,8 +1,9 @@
+# O(n^2) Time | O(n) Space
 def wordBreak(s, wordDict):
     words = set(wordDict)
     cache = {}
     getWordBreaks(s, words, 0, cache)
-    print(cache)
+    # print(cache)
     if 0 in cache:
         return cache[0]
     return False
@@ -23,8 +24,21 @@ def getWordBreaks(string, words, idx, cache):
     cache[idx] = hasWordBreaks
     return cache[idx]
 
+# O(n^2) Time | O(n) Space
+def wordBreakBottomUp(string, wordDict):
+    words = set(wordDict)
+    cache = [False for _ in range(len(string) + 1)]
+    cache[0] = True
+    
+    for i in range(1, len(cache)):
+        for j in range(i):
+            prefix = string[j : i]
+            if cache[j] and prefix in words:
+                cache[i] = True
+    return cache[-1]
+
 
 if __name__ == "__main__":
     s = "abcd"
     wordDict = ["a", "abc", "b", "cd"]
-    print(wordBreak(s, wordDict))
+    print(wordBreak(s, wordDict) == wordBreakBottomUp(s, wordDict) )
