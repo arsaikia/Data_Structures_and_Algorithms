00405917defb0fa4e37ae62c731546c351ce13f7
arsaikia <arunabh.matrix@gmail.com>
2020-11-13 21:04:55 -0600

    # Average:  O(n log n) | O(log n) Space

--- Data Structures and Algorithms/Python/Revision/Revision_Easy_3.py
+++ Data Structures and Algorithms/Python/Revision/Revision_Easy_3.py
@@ -15,7 +15,7 @@ def twoSum(array, target):
 
 
 # O(n) Time | O(1) Space
-def validateSubsequence( array, sequence ):
+def validateSubsequence(array, sequence):
     arrayIdx = sequenceIdx = 0
 
     while arrayIdx < len(array) and sequenceIdx < len(sequence):
@@ -25,7 +25,26 @@ def validateSubsequence( array, sequence ):
     return sequenceIdx == len(sequence)
 
 
+def findClosestValueInBst(tree, target):
+    return findClosestValueHelper(tree, target, float("inf"))
+
+
+def findClosestValueHelper(node, target, closest):
+    if node is None:
+        print(node, closest)
+        return closest
+    if abs(node.value - target) < abs(target - closest):
+        closest = node.value
+    if target > node.value:
+        return findClosestValueHelper(node.right, target, closest)
+    elif target < node.value:
+        return findClosestValueHelper(node.left, target, closest)
+    else:
+        return closest
 
 
 if __name__ == "__main__":
-    pass
+    from binarytree import bst as tree
+    bstTree = tree(is_perfect=True)
+    print(bstTree)
+    print(findClosestValueInBst(bstTree, 17))
