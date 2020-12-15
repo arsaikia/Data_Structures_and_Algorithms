7c63a9d9cee6caf58585740b1e42d466ef2765a3
arsaikia <arunabh.matrix@gmail.com>
2020-12-14 22:21:08 -0600

    .

--- Data Structures and Algorithms/Python/Revision/Revision_Medius_4.py
+++ Data Structures and Algorithms/Python/Revision/Revision_Medius_4.py
@@ -190,6 +190,78 @@ def getAllPeaks(arr):
     return peaks
 
 
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        curr = self
+        while True:
+            if value < curr.value:
+                if curr.left is None:
+                    curr.left = BST(value)
+                    break
+                else:
+                    curr = curr.left
+            else:
+                if curr.right is None:
+                    curr.right = BST(value)
+                    break
+                else:
+                    curr = curr.right
+        return self
+
+    def contains(self, value):
+        curr = self
+        while curr is not None:
+            if value < curr.value:
+                curr = curr.left
+            elif value > curr.value:
+                curr = curr.right
+            else:
+                return True
+        return False
+
+    def remove(self, value, parent=None):
+        curr = self
+        while curr is not None:
+            if value < curr.value:
+                parent = curr
+                curr = curr.left
+            elif value > curr.right:
+                parent = curr
+                curr = curr.right
+            else:
+                if curr.left is not None and curr.right is not None:
+                    curr.value = curr.right.getMinValue()
+                    curr.right.remove(curr.value, curr)
+                elif parent is None:
+                    if curr.left is not None:
+                        curr.value = curr.left.value
+                        curr.right = curr.left.right
+                        curr.left = curr.left.left
+                    elif curr.right is not None:
+                        curr.value = curr.right.value
+                        curr.left = curr.right.left
+                        curr.right = curr.right.right
+                    else:
+                        curr = None
+                elif parent.left == curr:
+                    curr.left = curr.left if curr.left is not None else curr.right
+                elif parent.right == curr:
+                    curr.right = curr.left if curr.left is not None else curr.right
+                break
+            return
+
+    def getMinValue(self):
+        curr = self
+        while curr.left is not None:
+            curr = curr.left
+        return curr.value
+
+
 if __name__ == "__main__":
     # array = [12, 3, 1, 2, -6, 5, -8, 6, 7]
     # target = 0
