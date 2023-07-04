479fee68b579668515f7738e7bf2924a2b5a99a3
arsaikia <arunabh.matrix@gmail.com>
2023-07-04 02:17:12 -0700

    valid-anagram

---
+++ prepalgo/Arrays & Hashing/valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    # O(N) Time | O(N) Space
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        sCount = [0 for __ in range(26)]
+        tCount = [0 for __ in range(26)]
+
+        for char in s:
+            idx = ord(str(char)) - ord("a")
+            sCount[idx] += 1
+
+        for char in t:
+            idx = ord(str(char)) - ord("a")
+            tCount[idx] += 1
+
+        return sCount == tCount
