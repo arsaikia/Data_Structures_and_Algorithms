bde3e5d3642a0ce18b3c3eb0041b10c4472057fa
arsaikia <arunabh.matrix@gmail.com>
2023-07-05 23:16:34 -0700

    Invert tree

--- prepalgo/Trees/invert-binary-tree.py
+++ prepalgo/Trees/invert-binary-tree.py
@@ -14,3 +14,23 @@ class Solution:
             return node
 
         return recursivePreorder(root)
+
+
+class Solution:
+    # O(N) Time
+    # O(H) Space  | Worst case Unbalanced Tree -> O(N)
+    #             | Best case Balanced Tree -> O(N/2)
+    def invertTreeIterativeBFS(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        def BFS(root):
+            q = collections.deque([root])
+
+            while q:
+                node = q.popleft()
+                if node:
+                    node.left, node.right = node.right, node.left
+                    q.append(node.left)
+                    q.append(node.right)
+            return root
+
+        return BFS(root)
