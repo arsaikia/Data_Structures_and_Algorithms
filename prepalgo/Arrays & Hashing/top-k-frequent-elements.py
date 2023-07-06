82379af10f99e025259232d4833bfb6cfe038003
arsaikia <arunabh.matrix@gmail.com>
2023-07-05 21:35:19 -0700

    top k frequent elements

---
+++ prepalgo/Arrays & Hashing/top-k-frequent-elements.py
@@ -0,0 +1,18 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        numsCounter = collections.Counter(nums)
+        result = []
+
+        buckets = [[] for i in range(len(nums) + 1)]
+
+        for num, count in numsCounter.items():
+            buckets[count].append(num)
+
+        for idx in reversed(range(len(buckets))):
+            bucket = buckets[idx]
+            for num in bucket:
+                if k > 0:
+                    result.append(num)
+                    k -= 1
+
+        return result
