b8a8e880b62625cec35ff124800d0d01c27c8ff6
arsaikia <arunabh.matrix@gmail.com>
2023-08-18 23:22:50 -0700

    Num of Islands - DFS and BFS

--- prepalgo/Graphs/number-of-islands.py
+++ prepalgo/Graphs/number-of-islands.py
@@ -1,18 +1,25 @@
+from collections import deque
 # O(m x n) Time | O(m x n) Space
-def numOfIslands(grid):
+
+###############################################################
+# DFS
+###############################################################
+
+
+def numOfIslandsDFS(grid):
     visited = [[False for num in row] for row in grid]
     islands = []
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if visited[row][col]:
                 continue
-            size = traverseNode(row, col, grid, visited)
+            size = traverseNodeDFS(row, col, grid, visited)
             if size:
                 islands.append(size)
     return islands
 
 
-def traverseNode(row, col, matrix, visited):
+def traverseNodeDFS(row, col, matrix, visited):
 
     if (row not in range(len(matrix)) or col not in range(len(matrix[0])) or visited[row][col]):
         return 0
@@ -21,10 +28,63 @@ def traverseNode(row, col, matrix, visited):
 
     if matrix[row][col] == "0":
         return 0
-    return 1 + traverseNode(row + 1, col, matrix, visited) \
-        + traverseNode(row - 1, col, matrix, visited) \
-        + traverseNode(row, col + 1, matrix, visited) \
-        + traverseNode(row, col - 1, matrix, visited)
+    return (1 + traverseNodeDFS(row + 1, col, matrix, visited)
+            + traverseNodeDFS(row - 1, col, matrix, visited)
+            + traverseNodeDFS(row, col + 1, matrix, visited)
+            + traverseNodeDFS(row, col - 1, matrix, visited))
+
+###############################################################
+# BFS
+###############################################################
+
+
+def numOfIslandsBFS(grid):
+    islands = []
+    visited = [[False for __ in row] for row in grid]
+
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if visited[row][col]:
+                continue
+            islandSize = traverseNodeBFS(grid, row, col, visited)
+            if islandSize:
+                islands.append(islandSize)
+    return islands
+
+
+def traverseNodeBFS(grid, row, col, visited):
+    ROW, COL, islandSize = len(grid), len(grid[0]), 0
+    nodesToTraverse = deque([[row, col]])
+
+    while nodesToTraverse:
+        row, col = nodesToTraverse.popleft()
+
+        if (
+            row not in range(ROW) or
+            col not in range(COL) or
+            visited[row][col]
+        ):
+            continue
+
+        visited[row][col] = True
+
+        if grid[row][col] == "0":
+            continue
+
+        islandSize += 1
+        for r, c in getNeighbors(row, col):
+            nodesToTraverse.append([r, c])
+    return islandSize
+
+
+def getNeighbors(row, col):
+    neighbors = []
+    directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+    for dr, dc in directions:
+        neighbors.append([row + dr, col + dc])
+
+    return neighbors
 
 
 grid = [
@@ -34,4 +94,5 @@ grid = [
     ["0", "0", "0", "0", "1"]
 ]
 
-print(numOfIslands(grid))
+print(numOfIslandsDFS(grid))
+print(numOfIslandsBFS(grid))
