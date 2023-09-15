54f5811a86db7864de4e305a5450ab47969eb4f2
arsaikia <arunabh.matrix@gmail.com>
2023-09-15 00:10:57 -0700

    Max Depth of Tree

--- prepalgo/Trees/maximum-depth-of-binary-tree.py
+++ prepalgo/Trees/maximum-depth-of-binary-tree.py
@@ -1,6 +1,6 @@
 class Solution:
     # DFS - Post-order
-    # O(N) Time | Best - O(N/2), Worst - O(N) Space
+    # O(N) Time | O(H) Space
     def maxDepth(self, root: Optional[TreeNode]) -> int:
 
         if not root:
@@ -9,3 +9,22 @@ class Solution:
         rightNodeCount = self.maxDepth(root.right)
 
         return 1 + max(leftNodeCount, rightNodeCount)
+
+########################################################
+
+# DFS - Pre-order
+# O(N) Time | O(H) Space
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        def getNodeCount(node, depth):
+            if not node:
+                return depth
+
+            left = getNodeCount(node.left, depth + 1)
+            right = getNodeCount(node.right, depth + 1)
+            return max(left, right)
+
+        return getNodeCount(root, 0)
