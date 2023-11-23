5bb4284835fb0faa6958f6061896a227d3672e6f
arsaikia <arunabh.matrix@gmail.com>
2023-11-22 22:29:00 -0800

    sols

--- prepalgo/Arrays & Hashing/trapping-rain-water.py
+++ prepalgo/Arrays & Hashing/trapping-rain-water.py
@@ -1,48 +1,24 @@
-# O(N) Time | O(N) Space
-class Solution:
-    def trap(self, height: List[int]) -> int:
-        maxheightToLeft = [0 for __ in height]
-        maxheightToRight = [0 for __ in height]
-
-        for i in range(1, len(height)):
-            maxheightToLeft[i] = max(maxheightToLeft[i-1], height[i-1])
-
-        for i in reversed(range(len(height) - 1)):
-            maxheightToRight[i] = max(maxheightToRight[i+1], height[i+1])
-
-        waterArea = 0
-
-        for idx in range(len(height)):
-            maxToLeft = maxheightToLeft[idx]
-            maxToRight = maxheightToRight[idx]
-            waterCapacityAtIdx = min(maxToLeft, maxToRight) - height[idx]
-            waterArea = (
-                waterArea + waterCapacityAtIdx) if waterCapacityAtIdx > 0 else waterArea
-
-        return waterArea
-
-
-##########################################################
 # O(N) Time | O(1) Space
 class Solution:
     def trap(self, height: List[int]) -> int:
-        waterArea = 0
-        left, right = 0, len(height) - 1
         leftMax, rightMax = 0, 0
+        l, r = 0, len(height) - 1
+        res = 0
 
-        while left < right:
-            leftElevation, rightElevation = height[left], height[right]
+        while l < r:
+            left, right = height[l], height[r]
+            minCommonHeight = min(leftMax, rightMax)
+            waterLeft = minCommonHeight - left
+            waterRight = minCommonHeight - right
+            res = res + waterLeft if waterLeft > 0 else res
+            res = res + waterRight if waterRight > 0 else res
 
-            if leftElevation < rightElevation:
-                if leftElevation > leftMax:
-                    leftMax = leftElevation
-                else:
-                    waterArea += leftMax - leftElevation
-                left += 1
+            leftMax = max(leftMax, left)
+            rightMax = max(rightMax, right)
+
+            if left < right:
+                l += 1
             else:
-                if rightElevation > rightMax:
-                    rightMax = rightElevation
-                else:
-                    waterArea += rightMax - rightElevation
-                right -= 1
-        return waterArea
+                r -= 1
+        
+        return res
